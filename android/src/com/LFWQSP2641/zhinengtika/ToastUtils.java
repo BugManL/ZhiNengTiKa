2d7404242dce7581f8de85deed63e251554dd4c3
LFWQSP2641 <liuyunbo2641@gmail.com>
2024-07-30 11:35:15 +0800

    fix.android14不显示Toast

--- android/src/com/LFWQSP2641/zhinengtika/ToastUtils.java
+++ android/src/com/LFWQSP2641/zhinengtika/ToastUtils.java
@@ -39,56 +39,8 @@ public class ToastUtils extends org.qtproject.qt.android.bindings.QtActivity {
                 //后setText 兼容小米默认会显示app名称的问题
                 Toast toast = Toast.makeText(activity, null, duration);
                 toast.setText(message);
-                if (isNotificationEnabled(activity)) {
-                    toast.show();
-                } else {
-                    showSystemToast(toast);
-                }
+                toast.show();
             }
         });
-    }
-
-    /**
-     * 显示系统Toast
-     */
-    private static void showSystemToast(Toast toast) {
-        try {
-            Method getServiceMethod = Toast.class.getDeclaredMethod("getService");
-            getServiceMethod.setAccessible(true);
-            //hook INotificationManager
-            if (iNotificationManagerObj == null) {
-                iNotificationManagerObj = getServiceMethod.invoke(null);
-
-                Class iNotificationManagerCls = Class.forName("android.app.INotificationManager");
-                Object iNotificationManagerProxy = Proxy.newProxyInstance(toast.getClass().getClassLoader(), new Class[]{iNotificationManagerCls}, new InvocationHandler() {
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        //强制使用系统Toast
-                        if ("enqueueToast".equals(method.getName())
-                                || "enqueueToastEx".equals(method.getName())) {  //华为p20 pro上为enqueueToastEx
-                            args[0] = "android";
-                        }
-                        return method.invoke(iNotificationManagerObj, args);
-                    }
-                });
-                Field sServiceFiled = Toast.class.getDeclaredField("sService");
-                sServiceFiled.setAccessible(true);
-                sServiceFiled.set(null, iNotificationManagerProxy);
-            }
-            toast.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 消息通知是否开启
-     *
-     * @return
-     */
-    private static boolean isNotificationEnabled(QtActivity activity) {
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(activity);
-        boolean areNotificationsEnabled = notificationManagerCompat.areNotificationsEnabled();
-        return areNotificationsEnabled;
     }
 }
